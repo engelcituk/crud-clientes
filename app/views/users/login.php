2068efc55c2a20185a340bc0506b08afc9eabf68
cituk <programaciondyn3.mx@sandos.com>
2021-11-21 13:32:52 -0500

    login fix

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,34 +1,25 @@
-<?php require APPROOT . '/views/shared/header.php'; ?>
-    <div class="row">
-        <div class="col-md-6 mx-auto">
-            <div class="card card-body bg-light mt-5">
-                <?php  flash('register_success');?>
-                <h2>Iniciar sesión</h2>
-                <p>Por favor ingrese su correo y contraseña</p>
-                <form action="<?php echo URLROOT . '/users/login'; ?>" method="post">
-                    <div class="form-group">
-                        <label for="email">Email: <sup>*</sup></label>
-                        <input type="email" name="email" class="form-control <?php echo (!empty($data['email_err'])) ? 'is-invalid':''; ?>" placeholder="Su correo electrónico" value="<?php echo $data['email'];?>">
-                        <span class="invalid-feedback"><?php echo $data['email_err'];?></span>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Contraseña: <sup>*</sup></label>
-                        <input type="password" name="password" class="form-control <?php echo (!empty($data['password_err'])) ? 'is-invalid':''; ?>" placeholder="Su contraseña" value="<?php echo $data['password'];?>">
-                        <span class="invalid-feedback"><?php echo $data['password_err'];?></span>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <a href="<?php echo URLROOT . '/users/register'; ?>">¿No tienes cuenta?, Regístrese</a>
-                        </div>
-                        <div class="col">
-                            <input type="submit" value="Iniciar sesión" class="btn btn-primary btn-block">
-                        </div>
-                    </div>
-                </form>
-            </div>
+<?php require APPROOT . '/views/shared/header2.php'; ?>
+
+<form class="login" action="<?php echo URLROOT . '/users/login'; ?>" method="post">
+        <div>
+            <h2 class="h2D">Digital Plus</h2>
         </div>
-    </div>
-<?php require APPROOT . '/views/shared/footer.php'; ?>
+        <div class="form-group">
+            <i class="fas fa-user"></i>
+            <!-- <input type="text" name="admin" placeholder="Usuario"><br> -->
+            <input type="email" name="email" class="form-control <?php echo ( !empty( $data['email_err'] ) ) ? 'is-invalid':''; ?>" placeholder="Su correo electrónico" value="<?php echo  !empty( $data['email'] ) ?  $data['email']: '';?>"><br>
+            <span class="invalid-feedback"><?php echo !empty( $data['email_err'] ) ? $data['email_err'] : '' ;?></span>
+        </div><br>
+        <div class="form-group">
+            <i class="fas fa-key"></i>
+            <input type="password" name="password" class="form-control <?php echo (!empty($data['password_err'])) ? 'is-invalid':''; ?>" placeholder="Su contraseña" value="<?php echo !empty( $data['password'] ) ? $data['password'] : '' ;?>"><br>
+            <span class="invalid-feedback"><?php echo !empty( $data['password_err'] ) ? $data['password_err'] : '';?></span>
+        </div><br>
 
+        <br><button type="submit"> Entrar </button><br><br>
+        <a href="<?php echo URLROOT . '/users/register'; ?>">¿No tienes cuenta?, Regístrese</a>
+    </form>
 
+</body>
 
+</html>
